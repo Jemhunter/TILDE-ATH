2506332039f279c6fe21ff6ad344a3d4cc296506
Jemhunter <goldmanrobbie@gmail.com>
2017-11-09 20:30:38 +0000

    Add files via upload
    Added version (To my forked version hopefully) of my edit - with the interface for allowing the user to kill objects manually

---
+++ interactiveVersion/Python2/editor.py
@@ -0,0 +1,48 @@
+#written by Jem_hunter
+import os
+
+running = True
+
+file = open("data\data.txt", "r+")
+file2 = open("data\data2.txt", "r+")
+
+while running:
+    
+    file2 = open("data\data2.txt", "r+")
+    inData = file2.read()
+    file2.close()
+    
+    
+    print "Enter name of deceased:"
+    inp = raw_input()
+    print
+    inp = inp.replace(" ", "")
+    
+    if inp == "?":
+        
+        file2 = open("data\data2.txt", "r+")
+        inData = file2.read()
+        inData = inData.replace("[", "").replace(" ", "").replace("'", "").replace('"', "").replace("]", "")
+        keys = inData.split(",")
+        file2.close()
+        
+        message = ""
+        for i in keys:
+            message = message + str(i) + " "
+        print message
+        print "Are curently alive"
+        print 
+        
+    if inp != "":
+
+        file = open("data\data.txt", "r+")
+        file.write(inp)
+        file.close()
+
+    if inp == "THIS":
+        running = False
+
+print "The program has finished so..."
+file.close()
+file2.close()
+os.system("pause")#needs to be modified for all OS
